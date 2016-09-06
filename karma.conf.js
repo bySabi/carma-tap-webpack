b4cf3a948b73858675b957b13dfe7f82fe12293f
bySabi Files <bysabi.files@gmail.com>
2016-09-06 14:12:59 +0200

    WIP

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests.webpack.js'
+      'tests1.webpack.js'
     ],
 
     // list of files to exclude
