7a299dd1ddb82cf99e31588dfdbf9ca5b6761543
bySabi Files <bysabi.files@gmail.com>
2016-09-28 20:34:40 +0200

    wip

---
+++ babel.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const JSON5 = require('json5');
+
+function getProjectBabelConfig(basePath) {
+  let json;
+  try {
+    const jsonFile = fs.readFileSync(path.resolve(basePath, '.babelrc'), 'utf8');
+    json = JSON5.parse(jsonFile);
+  } catch(e) {
+    if (e.code === 'ENOENT') {
+      console.log("file not found: " + e.path);
+      try {
+        const jsonFile = fs.readFileSync(path.resolve(basePath, 'package.json'), 'utf8');
+        json = JSON5.parse(jsonFile)['babel'];
+      } catch(e) {
+        console.error('Error parsing file: ', e.message);
+        throw e;
+      }
+    } else {
+      console.error('Error parsing file: ', e.message);
+      throw e;
+    }
+  }
+  return json;
+}
+
+function getCarmaBabelConfig(basePath) {
+  let json;
+  try {
+    const jsonFile = fs.readFileSync(path.resolve(basePath, 'query.json'), 'utf8');
+    json = JSON5.parse(jsonFile);
+  } catch(e) {
+    console.error('Error parsing file: ', e.message);
+    throw e;
+  }
+  return json;
+}
+
+function babelLoaderQuery(config) {
+  const basePath = config.basePath;
+  return updateOptions(
+    getProjectBabelConfig(basePath),
+    getCarmaBabelConfig(__dirname)
+  );
+}
+
+/*
+function updateOptions(babelConfig, babelExtend) {
+  if (babelConfig && babelExtend) {
+    const env = createEnvOptions()
+}
+*/
+
+function updateOptions(babelConfig, babelExtend) {
+  return Object.assign({}, babelConfig, babelExtend);
+}
+
+//  let config = objectAssign({}, babelConfig); // clone Babel settings
+//  delete config['env']; // remove 'env' property
+
+module.exports = babelLoaderQuery;
